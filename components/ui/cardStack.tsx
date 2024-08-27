42d83ad7220d3ea13392eac4b1ecc08dbb0cde6d
kyle-huynh23 <huynhkyle23@gmail.com>
2024-08-27 08:43:54 -0700

    Fixed bug where light computers on light mode didn't have a good experience.

--- components/ui/cardStack.tsx
+++ components/ui/cardStack.tsx
@@ -32,9 +32,8 @@ export const CardStack = ({
   }, []);
 
   const startFlipping = () => {
-    interval = setInterval(() => {
-      setCards((prevCards: Card[]) => {
-        // Maintain sorted order while flipping
+    const interval = setInterval(() => {
+      setCards((prevCards) => {
         const newArray = [...prevCards];
         newArray.unshift(newArray.pop()!); // Move the last element to the front
         return newArray;
@@ -43,36 +42,33 @@ export const CardStack = ({
   };
 
   return (
-    <div className="relative h-60 w-60 md:h-60 md:w-96">
-      {cards.map((card, index) => {
-        return (
-          <motion.div
-            key={card.id}
-            className="absolute dark:bg-black bg-white h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1] shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
-            style={{
-              transformOrigin: "top center",
-            }}
-            animate={{
-              top: index * -CARD_OFFSET,
-              scale: 1 - index * SCALE_FACTOR, // Decrease scale for cards that are behind
-              zIndex: cards.length - index, // Decrease z-index for the cards that are behind
-            }}
-          >
-            <div className="font-normal text-neutral-700 dark:text-neutral-200">
-              {card.content}
-            </div>
-            <div>
-              <p className="text-neutral-500 font-medium dark:text-white">
-                {card.name}
-              </p>
-              <p className="text-neutral-400 font-normal dark:text-neutral-200">
-                {card.designation}
-              </p>
-            </div>
-          </motion.div>
-        );
-      })}
+    <div className="relative h-60 w-60 md:h-60 md:w-96 "> {/* "dark" class added here */}
+      {cards.map((card, index) => (
+        <motion.div
+          key={card.id}
+          className="absolute bg-black  h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 border-white/[0.1] shadow-white/[0.05] flex flex-col justify-between"
+          style={{
+            transformOrigin: "top center",
+          }}
+          animate={{
+            top: index * -CARD_OFFSET,
+            scale: 1 - index * SCALE_FACTOR, // Decrease scale for cards that are behind
+            zIndex: cards.length - index, // Decrease z-index for the cards that are behind
+          }}
+        >
+          <div className="font-normal text-neutral-200">
+            {card.content}
+          </div>
+          <div>
+            <p className="text-neutral-500 text-white">
+              {card.name}
+            </p>
+            <p className="text-neutral-200">
+              {card.designation}
+            </p>
+          </div>
+        </motion.div>
+      ))}
     </div>
   );
 };
-
